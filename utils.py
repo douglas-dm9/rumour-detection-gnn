d6544d1a698325011dc0f0d7da077b3f51483d61
Douglas Melo dos Santos <douglasdm9@outlook.com>
2024-08-20 21:48:59 -0300

    Add files via upload

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ from torch_geometric.data import HeteroData
 import torch
 import torch_geometric.transforms as T
 import pickle
+from sklearn.preprocessing import RobustScaler
 
 class LoadRumoursDataset:
     def __init__(self, file_path_replies, file_path_posts, time_cut):
@@ -19,7 +20,8 @@ class LoadRumoursDataset:
         self.df_posts = pd.read_pickle(self.file_path_posts)
 
     def process_data(self):
-        post_features = ['followers','favorite_count','retweet_count','verified','rumour','id','embeddings_avg','text']
+        post_features = ['followers','favorite_count','retweet_count','verified','rumour','id','embeddings_avg']
+        
         reply_features = ['reply_followers','reply_user_id','reply_verified','time_diff','reply_embeddings_avg','id']
 
         filtered_replies = self.df_replies[reply_features][self.df_replies.time_diff < self.time_cut]
@@ -34,6 +36,18 @@ class LoadRumoursDataset:
         self.df_final['first_time_diff'] = self.df_final['first_time_diff'].fillna(0)
         self.df_final = self.df_final.drop(columns=['id'])
 
+        
+        # Initialize the Robust Scaler
+        scaler = RobustScaler()
+        
+        # Assuming data is a DataFrame containing your dataset
+        scaled_features = ['followers', 'favorite_count', 'retweet_count', 'first_time_diff']
+        # Convert the scaled features back to a DataFrame
+        scaled_data = pd.DataFrame(scaler.fit_transform(self.df_final [scaled_features]),columns=scaled_features)    
+        
+        self.df_final [scaled_features] = scaled_data
+
+
         # One-hot encoding
         self.df_final ['verified'] = self.df_final ['verified'].astype('str').str.\
                      replace(' ', '').replace('True', '1').replace('False', '0')\
@@ -46,6 +60,7 @@ class LoadRumoursDataset:
 
     def get_final_dataframe(self):
         return self.df_final
+
 
 
 class HeteroDataProcessor:
@@ -95,15 +110,34 @@ class HeteroDataProcessor:
 
         # Mapping reply user ids
         self.reply_user_map = {value: i for i, value in enumerate(self.df_replies['reply_user_id'].unique())}
-        self.df_replies["reply_user_id"] = self.df_replies["reply_user_id"].map(self.reply_user_map).astype(int)
+        self.df_replies["reply_user_id"] = self.df_replies["reply_user_id"].map(self.reply_user_map)
 
     def create_features(self):
         post_features = self.df_posts[["followers", "favorite_count", "retweet_count", "no_verified", "verified", "first_time_diff"]]
+
+
+        # Initialize the Robust Scaler
+        scaler = RobustScaler()
+        
+        # Assuming data is a DataFrame containing your dataset
+        scaled_features = scaler.fit_transform(post_features[['followers', 'favorite_count', 'retweet_count', 'first_time_diff']])
+        
+        # Convert the scaled features back to a DataFrame
+        scaled_data = pd.DataFrame(scaled_features, columns=['followers', 'favorite_count', 'retweet_count', 'first_time_diff'])
+        
+        # Add the binary features back to the scaled data
+        scaled_data['no_verified'] = post_features['no_verified']
+        scaled_data['verified'] = post_features['verified']
+        post_features = scaled_data
+
         post_embeddings = np.array(self.df_posts['embeddings_avg'].tolist())
         #post_features = self.scaler.fit_transform(post_features)
         x1 = np.concatenate((post_features, post_embeddings), axis=1)
 
-        reply_features = self.df_replies[["reply_followers", "reply_no_verified", "reply_verified", "time_diff"]]
+        scaler = RobustScaler()
+        reply_features = self.df_replies[["reply_followers", "reply_no_verified", "reply_verified","time_diff"]]
+        reply_features[['reply_followers','time_diff']] = scaler.fit_transform(reply_features[['reply_followers','time_diff']])
+
         reply_embeddings = np.array(self.df_replies['reply_embeddings_avg'].tolist())
         #reply_features = self.scaler.transform(reply_features)
         x2 = np.concatenate((reply_features, reply_embeddings), axis=1)
@@ -112,7 +146,7 @@ class HeteroDataProcessor:
 
     def create_heterodata(self, x1, x2):
         y = self.df_posts['rumour'].to_numpy()
-        edge_index = self.df_replies[["id", "reply_user_id"]].values.T
+        edge_index = self.df_replies[["id", "reply_user_id"]].values.transpose()
 
         num_rows = x1.shape[0]
         indices = np.arange(num_rows)
@@ -127,13 +161,13 @@ class HeteroDataProcessor:
         train_mask[train_indices], val_mask[val_indices], test_mask[test_indices] = True, True, True
 
         data = HeteroData()
-        data['twitter_id'].x = torch.tensor(x1, dtype=torch.float32)
-        data['twitter_id'].y = torch.tensor(y, dtype=torch.float32)
-        data['twitter_id'].train_mask = torch.tensor(train_mask, dtype=torch.bool)
-        data['twitter_id'].val_mask = torch.tensor(val_mask, dtype=torch.bool)
-        data['twitter_id'].test_mask = torch.tensor(test_mask, dtype=torch.bool)
+        data['id'].x = torch.tensor(x1, dtype=torch.float32)
+        data['id'].y =  torch.from_numpy(y)
+        data['id'].train_mask = torch.tensor(train_mask)
+        data['id'].val_mask = torch.tensor(val_mask) 
+        data['id'].test_mask = torch.tensor(test_mask)
         data['reply_user_id'].x = torch.tensor(x2, dtype=torch.float32)
-        data['twitter_id', 'retweet', 'reply_user_id'].edge_index = torch.tensor(edge_index, dtype=torch.long)
+        data['id', 'retweet', 'reply_user_id'].edge_index = torch.from_numpy(edge_index.reshape(2,len(x2)))
         data = T.ToUndirected()(data)
 
         return data
